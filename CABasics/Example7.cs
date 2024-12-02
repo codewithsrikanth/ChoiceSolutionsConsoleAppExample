4d4d3235bd8b56923063e5cd48184dbe2c6d7925
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-12-02 10:27:47 +0530

    Conditional Statements

---
+++ CABasics/Example7.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CABasics
+{
+    class Example7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter  3 numbers");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            int num2=Convert.ToInt32(Console.ReadLine());
+            int num3=Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+                Console.WriteLine("Num1 is Bigger");
+            else if (num2 > num3)
+                Console.WriteLine("Num2 is Bigger");
+            else
+                Console.WriteLine("Num3 is Bigger");
+
+
+
+            //if(num1>=num2 && num1>=num3)
+            // {
+            //     Console.WriteLine("Biggest number :" + num1);
+            // }
+
+            // else if (num2 >= num1 && num2 >= num3)
+            // {
+            //     Console.WriteLine("Biggest number :" + num2);
+            // }
+            // else 
+            // {
+            //     Console.WriteLine("Biggest number :" + num3);
+            // }
+
+            // if (num1 <= num2 && num1 <= num3)
+            // {
+            //     Console.WriteLine("smallest number :" + num1);
+            // }
+            // else if (num2 <= num1 && num2 <= num3)
+            // {
+            //     Console.WriteLine("Biggest number :" + num2);
+            // }
+
+            // else
+            // {
+            //     Console.WriteLine("Biggest number :" + num3);
+            // }
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ CABasics/Example8.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace CABasics
+{
+    class Example8
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any two numbers: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+        Operator:
+            Console.WriteLine("Enter an operator (+, -, /, *): ");
+            char op = Convert.ToChar(Console.ReadLine());
+            int res = 0;
+            //if(op == '+')
+            //    res = num1 + num2;
+            //else if(op == '-')
+            //    res = num1 - num2;
+            //else if(op == '*')
+            //    res = num1 * num2;
+            //else if(op == '/')
+            //    res = num1 / num2;
+            //else
+            //    Console.WriteLine("Invalid Operator");
+
+            bool isInvalid = false;
+            switch (op)
+            {
+                case '+':
+                    res = num1 + num2;
+                    break;
+                case '-':
+                    res = num1 - num2;
+                    break;
+                case '*':
+                    res = num1 * num2;
+                    break;
+                case '/':
+                case '%':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Second Number can't be zero");
+                        isInvalid = true;
+                    }
+                    else
+                        res = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    isInvalid = true;
+                    goto Operator;
+            }
+            if (isInvalid == false)
+                Console.WriteLine("Result is: " + res);
+            Console.Read();
+        
+        }
+    }
+}
