764758cc912f627a5f45a795e3618d4a30abf0ed
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-12-05 10:41:17 +0530

    Constructors

---
+++ CAConstrcutors/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CAConstrcutors
+{
+    //Class
+    class Employee
+    {
+        int empId; string empName; double salary;
+        string city;string state;
+        //User-Defined Default Constructor
+        public Employee()
+        {
+            empId = 101;
+            empName = "Srikanth";
+            salary = 12345.67;
+        }
+        //Parameter Constructor
+        public Employee(int empId, string empName, double sal)
+        {
+            //this - current class instance/object
+            this.empId = empId;
+            this.empName = empName;
+            salary = sal;
+        }
+        public Employee(string city, string state)
+        {
+            this.state = state;
+            this.city = city;
+        }
+        //Copy Construtor
+        public Employee(string empName, Employee obj)
+        {
+            this.empName= empName;
+            this.city = obj.city;
+            this.state = obj.state;
+        }
+        //Method
+        public void Display()
+        {
+            Console.WriteLine($"EmpID: {empId}, EmpName: {empName} and Salary: {salary}");
+            Console.WriteLine($"City: {city} and State: {state}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee address = new Employee("Hyderabad", "Telangana");
+
+
+            Employee emp = new Employee();
+            emp.Display();
+
+            Employee emp1 = new Employee(102, "Hari", 87654.98);
+            emp1.Display();
+
+            Employee emp2 = new Employee("Diyansh", address);
+            emp2.Display();
+            
+
+            Console.Read();
+        }
+    }
+}
---
+++ CAOopsExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CAOopsExample
+{
+    class Product
+    {
+        int productId;string productName;double price;int qty;
+        //Method
+        public void GetProduct()
+        {
+            Console.WriteLine("Enter Product Details: ");
+            productId = Convert.ToInt32(Console.ReadLine());
+            productName = Console.ReadLine();
+            price = Convert.ToDouble(Console.ReadLine());
+            qty = Convert.ToInt32((Console.ReadLine()));
+        }
+        //Function
+        public double GetTotalCost()
+        {
+            double total = price * qty;
+            return total;
+        }
+        public void Display()
+        {
+            double total = GetTotalCost();
+            Console.WriteLine($"Product ID: {productId}, Product Name: {productName} and Total value is: {total}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product pObj = new Product();
+            pObj.GetProduct();
+            pObj.Display();
+            Console.Read();
+        }
+    }
+}
