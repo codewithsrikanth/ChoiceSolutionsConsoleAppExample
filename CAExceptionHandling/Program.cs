1092c439dd4f952529e1b2368a0648c269ff34cf
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-12-14 11:46:43 +0530

    Exception handling

---
+++ CAExceptionHandling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CAExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = 0;
+            try
+            {                
+                Console.WriteLine("Enter two numbers: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Second number can't be zero");
+                }
+                else
+                {
+                    res = num1 / num2;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            //catch
+            //{
+            //    Console.WriteLine("Exception occured");
+            //}
+            finally
+            {
+                Console.WriteLine("Result is: " + res);
+                Console.Read();
+            }          
+           
+        }
+    }
+}
