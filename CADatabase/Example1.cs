f9beac7d7531aecb05fc15a6f176429068fea0b1
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-12-18 11:09:46 +0530

    ADO.Net Day-1

---
+++ CADatabase/Example1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CADatabase
+{
+    class Example1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connstr = "server=.;integrated security=true;database=CompanyDB";
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = connstr;
+                con.Open();
+                Console.WriteLine("Connection Opened");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Console.Read();
+            }
+           
+        }
+    }
+}
---
+++ CADatabase/Example2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CADatabase
+{
+    class Example2
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Estb Connection
+                SqlConnection con = new SqlConnection("server=.;integrated security=true;database=CompanyDB");
+
+                //Prepare the query and pass
+                Console.WriteLine("Enter Eno: ");
+                int eno = Convert.ToInt32(Console.ReadLine());
+                string query = "delete from Employee where Eno=" + eno;
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                //Open Connection
+                con.Open();
+
+                //Execute Query
+                int rowCount = cmd.ExecuteNonQuery();
+
+                //Close connection
+                con.Close();
+
+                if (rowCount > 0)
+                {
+                    Console.WriteLine("Record Deleted");
+                }
+                else
+                {
+                    Console.WriteLine("No Record found");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            Console.Read();
+        }
+    }
+}
