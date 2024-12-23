704c3ec7bc8908448617325a40d0753d883f7322
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-12-23 10:24:32 +0530

    ADO.Net Day-4

---
+++ CADatabase/Example7.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CADatabase
+{
+    class Example7
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ChoiceConn"].ConnectionString)) 
+            {                
+                SqlDataAdapter da = new SqlDataAdapter("select * from Employee", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                Console.WriteLine("Employee Details are: ");
+                foreach (DataRow dr in dt.Rows)
+                {
+                    Console.WriteLine($"{dr["Eno"]} -   {dr["Ename"]}   -   {dr["Job"]} -   {dr["Salary"]}  -   {dr["Dname"]}");
+                }
+
+                Console.WriteLine("Dataset Example");
+                DataSet ds = new DataSet();
+                da.Fill(ds, "EmpDetails");
+                foreach (DataRow dr in ds.Tables["EmpDetails"].Rows)
+                {
+                    Console.WriteLine($"{dr["Eno"]} -   {dr["Ename"]}   -   {dr["Job"]} -   {dr["Salary"]}  -   {dr["Dname"]}");
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ CADatabase/Example8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CADatabase
+{
+    class Example8
+    {
+        static void Main(string[] args)
+        {
+            //Step-1
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ChoiceConn"].ConnectionString))
+            {
+                //Step-2
+                SqlDataAdapter da = new SqlDataAdapter("select * from Employee", con);
+                //3
+                DataSet ds = new DataSet();
+                SqlCommandBuilder bldr = new SqlCommandBuilder(da);               
+                da.Fill(ds, "Employee");
+
+                foreach (DataRow dr in ds.Tables["Employee"].Rows)
+                {
+                    Console.WriteLine($"{dr["Eno"]} -   {dr["Ename"]}   -   {dr["Job"]} -   {dr["Salary"]}  -   {dr["Dname"]}");
+                }
+                //4
+                //Insert
+                DataRow row = ds.Tables["Employee"].NewRow();
+                row["Eno"] = 117;
+                row["Ename"] = "Abhi";
+                row["Job"] = "Admin";
+                row["Salary"] = 1234.5;
+                row["Dname"] = "Administration";
+                ds.Tables["Employee"].Rows.Add(row);
+
+                //5
+                if (ds.HasChanges())
+                {
+                    da.Update(ds.Tables["Employee"]);
+                    Console.WriteLine("Record(s) updated on DB");
+                }
+
+            }
+            Console.Read();
+        }
+    }
+}
